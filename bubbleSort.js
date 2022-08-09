09ebd4d04258685a95115b217cc12f6f9a447548
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-08-09 15:00:44 -0700

    bubble sort optimization

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,12 +3,16 @@ const swap = (arr, idx1, idx2) => {
 };
 
 function bubbleSort(arr) {
+ let noSwaps;
  for (let i = arr.length - 1; i >= 0; i--) {
+  noSwaps = true;
   for (let j = 0; j <= i - 1; j++) {
    if (arr[j] > arr[i]) {
     swap(arr, i, j);
+    noSwaps = false;
    }
   }
+  if (noSwaps) break;
  }
  return arr;
 }
