bdbacce365eb3f9bcb2dbbae61e3e9d311b63063
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-08-10 16:22:26 -0700

    pop()LL

---
+++ DATA STRUCTURES/Learn.js
@@ -0,0 +1,9 @@
+//Arrays: practice basic which involve brute force, then graudally start solving same with better time complexity with one traversal. Sorting and Searching algos
+//LinkedList: Improves logical skills of pointers. Start iwth Linked list then eventually move to double
+//////
+
+//Binary Tree: One of the most important topics. Maximum time solving problems here
+//Hashmap and Backtracking/Recursion: backbone of DSA = recursion. Be patient, many can be solved in O(1) using hashmap
+
+//Graph: create using adjacency list. Once you can create, move to Depth first Search and then Breadth first search.
+//Dynamic Programming: top company interview, especially non CS degree
--- DATA STRUCTURES/SinglyLinkedList.js
+++ DATA STRUCTURES/SinglyLinkedList.js
@@ -24,24 +24,50 @@ class SinglyLinkedList {
    this.head = newNode;
    this.tail = this.head;
   } else {
-   this.tail.next = newNode;
-   this.tail = newNode;
+   this.tail.next = newNode;//put new node at end
+   this.tail = newNode;//make the tail the new node
   }
   this.length++;
   return this;
  }
 
  traverse() {
-  var current = this.head;
+  let current = this.head;
   while (current) {
    current = current.next;
   }
  }
+
+ pop() {
+  if (!this.head) return undefined;
+  if (this.length === 1) {
+   this.head = null;
+   this.tail = null;
+  }
+
+  let current = this.head;
+  let newTail = current;//start together beore loop
+  //loop over, while there is a next prop on current
+  while (current.next) {
+   newTail = current;//new tail is always lagging behind one, until nowhere to move forward to
+   current = current.next;//move current foward one
+  }//at end, current.val
+  this.tail = newTail;
+  this.tail.next = null;
+  this.length--;
+  return current;
+
+ }
+
 }
-
-
 const list = new SinglyLinkedList();
 
 list.push('HEYYYYY');
 list.push('ASHLEY');
+list.push('WHATSUP');
+list.push('1');
+list.push('2');
 console.log(list);
+list.pop();
+console.log(list);
+
