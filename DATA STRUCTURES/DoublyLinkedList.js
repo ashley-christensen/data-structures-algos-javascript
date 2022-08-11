a9c50a79606e4e3f9c16cc7bfa4768f30036c5c9
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-08-11 16:27:00 -0700

    pop doubly linked list

--- DATA STRUCTURES/DoublyLinkedList.js
+++ DATA STRUCTURES/DoublyLinkedList.js
@@ -12,4 +12,44 @@ class DoublyLinkedList {
   this.tail = null;
   this.length = 0;
  }
+
+ addToTail(val) {
+  let newNode = new Node(val);
+  if (!this.head) {
+   this.head = newNode;
+   this.tail = newNode;
+  } else {
+   this.tail.next = newNode;
+   newNode.prev = this.tail;//set the .prev from new node back to old tail
+   this.tail = newNode;//update tail to be new node.\
+   this.length++;
+   return this;
+  }
+ }
+
+ removeFromTail() {
+  if (!this.head) return undefined;
+  let poppedNode = this.tail;
+  if (this.length === 1) {
+   this.head = null;
+   this.tail = null;
+  }
+
+  this.tail = poppedNode.prev;
+  this.tail.next = null;
+  poppedNode.prev = null;
+
+  this.length--;
+  return poppedNode;
+
+
+
+ }
 }
+
+const list = new DoublyLinkedList();
+list.addToTail(99);
+list.addToTail(98);
+list.addToTail(97);
+console.log(list.removeFromTail());
+// console.log(list);
