4cf8fee2fd4754823c269821cf2882f715f39b3c
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-08-15 10:17:26 -0700

    factorials

--- ALGORITHMS/recursion.js
+++ ALGORITHMS/recursion.js
@@ -16,7 +16,7 @@ function sumRange(num) {
  };
  return num + sumRange(num - 1);
 }
-console.log(sumRange(3));
+// console.log(sumRange(3));
 
 
 function factorial(num) {
@@ -29,3 +29,24 @@ function factorial(num) {
  return num * factorial(num - 1);
 }
 // console.log(factorial(10));
+
+function factorial2(num) {
+ let total = 1;
+ for (let i = num; i > 1; i--) {
+  total = total * i;//from num down to one we multiply to prev output
+ }
+ return total;
+}
+
+// console.log(factorial2(10));
+
+function factorial3(num) {
+ if (num <= 1) {//if multiplied by 0, goes back to 0
+  console.log('in the if', num);
+  return 1;// want to return 1, should be 1, don't multiply by 0 or anything less
+ }
+ return num * factorial3(num - 1);//basic logic without a stopping point
+
+}
+
+console.log(factorial3(10));
