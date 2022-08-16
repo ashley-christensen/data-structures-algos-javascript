3475e398ecc240f43603b3d19b06e59fc52d28cc
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-08-16 15:05:05 -0700

    binary search tree contains()

--- DATA STRUCTURES/BinarySearchTree.js
+++ DATA STRUCTURES/BinarySearchTree.js
@@ -35,42 +35,45 @@ class BinarySearchTree {
    }
   }
  }
+ contains(val) {
+  if (this.root === null) return false;
 
- insert2(val) {
-  let node = new Node(val);
-  if (this.root === null) {
-   this.root = node;
-   return this;
-  }
   let current = this.root;
-  while (true) {
-   if (val === current.val) return undefined;
-   if (val < current.left) {
-    if (current.left === null) {
-     current.left = node;
-     return this;
-    }
+  let found = false;
+  while (current && !found) {//cannot update current and loop over, and have not found
+   if (val < current.val) {
     current = current.left;
    }
-   else if (val > current.right) {
-    if (current.right === null) {
-     current.right = node;
-     return this;
-    }
+   else if (val > current.val) {
     current = current.right;
+   } else {
+    return true;
    }
   }
+  return false;
  }
+ contains2(val) {
+  if (this.root === null) return false;
+  let current = this.root;
+  let found = false;
 
-
+  while (current && !found) {
+   if (val < current.val) {
+    current = current.left;
+   }
+   else if (val > current.val) {
+    current = current.right;
+   } else {
+    return true;
+   }
+  }
+  return false;
+ }
 }
 
 
-
-
 let tree = new BinarySearchTree();
-tree.insert2(12);
-tree.insert2(6);
-tree.insert2(3);
-tree.insert2(14);
-console.log(tree);
+tree.insert(12);
+tree.insert(6);
+tree.insert(13);
+console.log(tree.contains(1));
