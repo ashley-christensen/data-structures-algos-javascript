a07d804716cdd52e637ed9a772e53364dbda00b9
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-08-16 12:21:24 -0700

    insert Binary Search Tree

--- DATA STRUCTURES/BinarySearchTree.js
+++ DATA STRUCTURES/BinarySearchTree.js
@@ -1,11 +1,4 @@
 //Binary Search Tree
-
-class BinarySearchTree {
- constructor() {
-  this.root = null;
- }
-}
-
 class Node {
  constructor(val) {
   this.val = val;
@@ -13,3 +6,48 @@ class Node {
   this.right = null;
  }
 }
+class BinarySearchTree {
+ constructor() {
+  this.root = null;
+ }
+ insert(val) {
+  let node = new Node(val);
+  if (this.root === null) {
+   this.root = node;
+   return this;
+  } else {
+   let current = this.root;
+   while (true) {
+    if (val < current.val) {
+     if (current.left === null) {
+      current.left = node;
+      return this;
+     } else {
+      current = current.left;
+     }
+    }
+    else if (val > current.val) {
+     if (current.right === null) {
+      current.right = node;
+      return this;
+     } else {
+      current = current.right;
+     }
+    }
+   }
+  }
+ }
+
+
+
+}
+
+
+
+
+let tree = new BinarySearchTree();
+tree.insert2(12);
+tree.insert2(6);
+tree.insert2(3);
+tree.insert2(14);
+console.log(tree);
